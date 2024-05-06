1c4439827656b387ca5dcb1f32cf91b92555a67f
Susumu <51506454+Susumu2019@users.noreply.github.com>
2024-05-06 13:50:00 +0900

    Update M5StampC3_WebServer_20240506.ino

--- M5StampC3_WebServer_20240506.ino
+++ M5StampC3_WebServer_20240506.ino
@@ -1,83 +1,129 @@
 #include <WiFi.h>
-#include <WiFiClient.h>
 #include <WebServer.h>
-#include <ESPmDNS.h>
-
-const char* ssid = "aterm-0dfd8a-g";
-const char* password = "9e6b4a5f07432";
+#include <NTPClient.h>//NTPで使用
+#include <WiFiUdp.h>//NTPで使用
 
 WebServer server(80);
 
-const int led = 13;
+// WiFi情報
+const char* ssid = "aterm-0dfd8a-g";
+const char* pass = "9e6b4a5f07432";
 
-void handleRoot() {
-  //digitalWrite(led, 1);
+int8_t parameter = 0;
 
-  char* doc = "<a href=''>zeatec</a>";
-  //server.send(200,"text/plain", "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><meta name='viewport' content='width=device-width, initial-scale=1.0'><title>Document</title></head><body></body></html>");
+//NTPサーバー関係
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "ntp.nict.jp", 9*3600); // NICTのNTPサーバーを使用し、日本標準時(UTC+9)を指定
 
-  server.println("HTTP/1.1 200 OK");
-  server.println("Content-Type: text/html");
-  //server.println("<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><meta name='viewport' content='width=device-width, initial-scale=1.0'><title>Document</title></head><body>".doc."</body></html>");
-  server.println("Connection: close");
-  server.println("");
+// 初期画面
+void handleRoot(void){
+    String html;
 
-  //digitalWrite(led, 0);
+    // HTMLを組み立てる
+    html = "<!DOCTYPE html><html><head><meta charset='utf-8'>";
+    html += "<title>parameterを1/0する</title></head><body>";
+    html += "<p>リンクをクリックするとparameterが1/0します</p>";
+    html += "<a href='/set?p=1'>1</a><br>";
+    html += "<a href='/set?p=0'>0</a>";
+    html += "<form action='set' method='get'>";
+    html += "   <select name='p'>";
+    html += "     <option value='10'>10</option>";
+    html += "     <option value='11'>11</option>";
+    html += "   </select>";
+    html += "<input type='submit' value='Submit'>";
+    html += "</form>";
+    html += "</body></html>";
+
+
+    // HTMLを出力する
+    server.send(200, "text/html", html);
 }
 
-void handleNotFound() {
-  //digitalWrite(led, 1);
-  String message = "File Not Found\n\n";
-  message += "URI: ";
-  message += server.uri();
-  message += "\nMethod: ";
-  message += (server.method() == HTTP_GET) ? "GET" : "POST";
-  message += "\nArguments: ";
-  message += server.args();
-  message += "\n";
-  for (uint8_t i = 0; i < server.args(); i++) {
-    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
-  }
-  server.send(404, "text/plain", message);
-  //digitalWrite(led, 0);
+// LEDのON/OFF
+void handleSET(void){
+    String html;
+
+    // 「/led?s=○」のパラメータが指定されているかどうかを確認
+    if (server.hasArg("p"))
+    {
+
+        html = "<!DOCTYPE html><html><head><meta charset='utf-8'>";
+        html += "<title>parameterを1/0する</title></head><body>";
+
+        // 「○」の値に応じて、LEDをON/OFFする
+        if (server.arg("p").equals("1")){
+            parameter = 1;
+            //digitalWrite(4, HIGH);
+
+            html += "<p>parameterを1にしました</p>";
+        }else if (server.arg("p").equals("0")){
+            parameter = 0;
+            html += "<p>parameterを0にしました</p>";
+        }else if (server.arg("p").equals("10")){
+            parameter = 10;
+            html += "<p>parameterを10にしました</p>";
+        }else if (server.arg("p").equals("11")){
+            parameter = 11;
+            html += "<p>parameterを11にしました</p>";
+
+        }else{
+            html += "パラメータが正しく指定されていません";
+
+        }
+
+        html += "<a href='#' onclick='history.back(); return false;'>back</a>";
+        html += "</body></html>";
+
+        Serial.printf("parameter=%d\r\n",parameter);
+    }
+
+    // 変数msgの文字列を送信する
+    server.send(200, "text/html", html);
+    //server.send(200, "text/plain; charset=utf-8", msg);
 }
 
-void setup(void) {
-  // pinMode(led, OUTPUT);
-  // digitalWrite(led, 0);
-  Serial.begin(115200);
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-  Serial.println("Start HelloServer");
-
-  // Wait for connection
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println(ssid);
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
-
-  if (MDNS.begin("esp32")) {
-    Serial.println("MDNS responder started");
-  }
-
-  server.on("/", handleRoot);
-
-  server.on("/inline", []() {
-    server.send(200, "text/plain", "this works as well");
-  });
-
-  server.onNotFound(handleNotFound);
-
-  server.begin();
-  Serial.println("HTTP server started");
+// 存在しないアドレスが指定された時の処理
+void handleNotFound(void){
+    server.send(404, "text/plain", "Not Found.");
 }
 
-void loop(void) {
-  server.handleClient();
-  delay(2);//allow the cpu to switch to other tasks
+// 初期化
+void setup()
+{
+    // シリアルポートの初期化
+    Serial.begin(115200);
+
+    // WiFiのアクセスポイントに接続
+    WiFi.begin(ssid, pass);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+    }
+    // ESP32のIPアドレスを出力
+    Serial.println("WiFi Connected.");
+    Serial.print("IP = ");
+    Serial.println(WiFi.localIP());
+    Serial.printf("parameter=%d\r\n",parameter);
+
+    // Groveセンサーの初期化
+    //pinMode(4, OUTPUT);
+
+    // NTPサーバーに接続
+    timeClient.begin();
+    // 時刻の更新
+    timeClient.update();
+    // 時刻の取得
+    Serial.println(timeClient.getFormattedTime());    
+
+    // 処理するアドレスを定義
+    server.on("/", handleRoot);
+    server.on("/set", handleSET);
+    server.onNotFound(handleNotFound);
+    // Webサーバーを起動
+    server.begin();
+}
+
+// 処理ループ
+void loop()
+{
+    server.handleClient();
 }
